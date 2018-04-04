efed07526939b3cb68d33fb4258483bba8909f8a
heyefu <527380349@qq.com>
2018-04-04 13:31:29 +0800

    traverse fileList or it's son

---
+++ src/main/java/com/heyfu/practice/io/Travserse.java
@@ -0,0 +1,93 @@
+package com.heyfu.practice.io;
+
+import java.io.File;
+
+/**
+ * Description:
+ * <br>遍历
+ *
+ * @author heyefu
+ * Create in: 2018-04-04
+ * Time: 11:09
+ **/
+public class Travserse {
+
+    /**
+     * Description:
+     * <br>找出文件列表files及其子文件夹中的最大文件和不等于0的最小文件
+     *
+     * @param files   文件列表
+     * @param maxFile 最大文件
+     * @param minFile 最小文件
+     * @param travSon 是否遍历子文件夹
+     * @return java.io.File[]
+     * @author heyefu 13:13 2018/4/4
+     **/
+    public static File[] traversrFileList(File[] files, File maxFile, File minFile, Boolean travSon) {
+        if (files == null || files.length == 0) {
+            System.out.println("文件列表为空!");
+            return null;
+        }
+        for (File f : files) {
+            System.out.println(f.getName());
+//            初始化最大的文件和最小的文件
+            if (f.length() > 0 && maxFile == null) {
+                maxFile = f;
+            }
+            if (f.length() > 0 && minFile == null) {
+                minFile = f;
+            }
+//            如果该文件是文件夹且要求遍历子文件夹,则继续遍历
+            if (travSon && f.isDirectory()) {
+                File[] fileList = f.listFiles();
+                File[] maxMin = traversrFileList(fileList, maxFile, minFile, travSon);
+                if (maxMin != null && maxFile.length() < maxMin[0].length()) {
+                    maxFile = maxMin[0];
+                }
+                if (maxMin != null && minFile.length() > maxMin[1].length()) {
+                    minFile = maxMin[1];
+                }
+                continue;
+            }
+//            判断最大的文件和最小文件
+            if (maxFile != null && f.length() > 0 && maxFile.length() < f.length()) {
+                maxFile = f;
+            }
+            if (minFile != null && f.length() > 0 && minFile.length() > f.length()) {
+                minFile = f;
+            }
+        }
+
+        File[] maxMin = {maxFile, minFile};
+        return maxMin;
+    }
+
+
+    /**
+     * Description:
+     * <br>遍历文件列表找出最大文件及不等于0最小文件
+     *
+     * @param files 文件列表
+     * @return java.io.File[]
+     * @author heyefu 13:16 2018/4/4
+     **/
+    public static File[] getMaxAndMinFile(File[] files) {
+        return getMaxAndMinFile(files, false);
+    }
+
+    /**
+     * Description:
+     * <br>遍历文件列表及其子文件夹找出最大文件及不等于0的最小文件
+     *
+     * @param files   文件列表
+     * @param travSon 是否遍历子文件夹
+     * @return java.io.File[]
+     * @author heyefu 13:18 2018/4/4
+     **/
+    public static File[] getMaxAndMinFile(File[] files, Boolean travSon) {
+        File maxFile = null;
+        File minFile = null;
+        return traversrFileList(files, maxFile, minFile, travSon);
+    }
+
+}
--- src/test/java/com/heyfu/test/TestIo.java
+++ src/test/java/com/heyfu/test/TestIo.java
@@ -1,5 +1,6 @@
 package com.heyfu.test;
 
+import com.heyfu.practice.io.Travserse;
 import org.junit.Test;
 
 import java.io.File;
@@ -23,29 +24,32 @@ public class TestIo {
     @Test
     public void traverseFile() {
 
-        String filePath = "C:\\Windows";
+        String filePath = "C:\\Users\\Lenovo\\Desktop\\数据传输原型";
         File file = new File(filePath);
         if (!file.exists()) {
             System.out.println(filePath + "文件不存在");
             return;
         }
-
         File[] fileList = file.listFiles();
         File minFile = null;
         File maxFile = null;
         String format = "文件名: %s  文件大小: %.8f %n";
 
-        for (File f : fileList){
-            if (minFile == null && f.length() > 0){
+        File[] max_min = Travserse.getMaxAndMinFile(fileList, true);
+        System.out.printf("最大文件名:%s, 文件大小:%d, 文件路径:%s %n", max_min[0].getName(), max_min[0].length(), max_min[0].getAbsolutePath());
+        System.out.printf("最小文件名:%s, 文件大小:%d, 文件路径:%s %n", max_min[1].getName(), max_min[1].length(), max_min[1].getAbsolutePath());
+
+        for (File f : fileList) {
+            if (minFile == null && f.length() > 0) {
                 minFile = f;
             }
-            if (maxFile == null && f.length() > 0){
+            if (maxFile == null && f.length() > 0) {
                 maxFile = f;
             }
-            if (f.length() > 0 && minFile.length() > f.length()){
+            if (f.length() > 0 && minFile.length() > f.length()) {
                 minFile = f;
             }
-            if (f.length() > 0 && f.length() > maxFile.length()){
+            if (f.length() > 0 && f.length() > maxFile.length()) {
                 maxFile = f;
             }
         }
@@ -56,7 +60,6 @@ public class TestIo {
         System.out.printf("总共有%d个文件%n", fileList.length);
         System.out.printf(format, minFile.getName(), minLength);
         System.out.printf(format, maxFile.getName(), maxLength);
-
 
     }
 
