c7620d1cda8d4cecc9ff87f86107300afcaaf13a
heyefu <527380349@qq.com>
2018-04-11 21:10:58 +0800

    make no synchronized more obvious

--- src/test/java/com/heyfu/test/Concurrency.java
+++ src/test/java/com/heyfu/test/Concurrency.java
@@ -16,7 +16,7 @@ public class Concurrency {
     @Test
     public void testSynchronized() {
         final Hero gareen = new Hero("gareen", 1000f);
-        int threadNum = 1000;
+        int threadNum = 500;
         System.out.printf("gareen的初始生命值为:%.8f%n", gareen.getHp());
 
         Thread[] addThreads = new Thread[threadNum];
@@ -31,34 +31,43 @@ public class Concurrency {
             });
             t.start();
             addThreads[i] = t;
-        }
-//        减少生命的线程
-        for (int i = 0; i < threadNum; i++) {
-            Thread t = new Thread(new Runnable() {
+
+            Thread t_down = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     gareen.hurt();
                 }
             });
-            t.start();
-            reduceThreads[i] = t;
+            t_down.start();
+            reduceThreads[i] = t_down;
         }
+//        减少生命的线程
+//        for (int i = 0; i < threadNum; i++) {
+//            Thread t = new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    gareen.hurt();
+//                }
+//            });
+//            t.start();
+//            reduceThreads[i] = t;
+//        }
 
 //        确保所有线程走完
-        for (Thread t : addThreads) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        for (Thread t : reduceThreads){
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        for (Thread t : addThreads) {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        for (Thread t : reduceThreads){
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
         System.out.printf("%d个增加线程和%d个减少线程结束后gareen的生命值为%.8f%n", threadNum, threadNum, gareen.getHp());
 
 
