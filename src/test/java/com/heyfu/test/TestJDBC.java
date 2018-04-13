fc5e411778ca6b1c05edb003638ab4dd9b3625ba
heyefu <527380349@qq.com>
2018-04-13 13:29:46 +0800

    paging query

--- src/test/java/com/heyfu/test/TestJDBC.java
+++ src/test/java/com/heyfu/test/TestJDBC.java
@@ -1,6 +1,5 @@
 package com.heyfu.test;
 
-import com.mysql.jdbc.PreparedStatement;
 import org.junit.Test;
 
 import java.sql.*;
@@ -14,6 +13,48 @@ import java.sql.*;
  * Time: 11:33
  **/
 public class TestJDBC {
+
+
+    public static void main(String[] args) {
+        list(0, 2);
+        list(1,2);
+        list(2,2);
+
+    }
+
+
+    /**
+     * Description:
+     * 分页查询
+     *
+     * @param start 开始页数
+     * @param count 每页显示总数
+     * @author heyefu 13:12 2018/4/13
+     **/
+    public static void list(int start, int count) {
+
+        Connection conn = getConn();
+
+        try (Statement s = conn.createStatement()) {
+            ResultSet result = s.executeQuery("SELECT * FROM product_ limit " + start*count + ", " + count);
+            while (result.next()){
+                System.out.printf("%s %s %s %s%n", result.getString(1), result.getString(2), result.getString(3), result.getString(4));
+            }
+            result.last();
+            System.out.printf("总共有%d条记录!", result.getRow());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
     /**
@@ -44,6 +85,21 @@ public class TestJDBC {
             e.printStackTrace();
         }
 
+    }
+
+
+    public static Connection getConn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/how2java", "root", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
