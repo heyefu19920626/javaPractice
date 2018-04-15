14a48beea7c8fdf308cc103770d7640ef3c3c82e
heyefu <527380349@qq.com>
2018-04-15 14:44:15 +0800

    DataInputStream readUTF

--- src/main/java/internet/Server.java
+++ src/main/java/internet/Server.java
@@ -1,5 +1,6 @@
 package internet;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -16,30 +17,57 @@ public class Server {
 
 
     public static void main(String[] args) {
-        ServerSocket server = null;
-        Socket s = null;
-        try {
-            server = new ServerSocket(8888);
-            System.out.println("正在监听8888端口");
-            s = server.accept();
-            InputStream in = s.getInputStream();
-            int msg = in.read();
-            System.out.println(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                s.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                server.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
+        Server server = new Server();
+
+//        测试Socket接收int数据
+//        server.acceptInt();
+
+//        测试Socket接收字符串
+        server.acceptString();
+
+
     }
 
 
+    /**
+     * Description:
+     * 利用DataOutputStream接收字符串
+     *
+     * @author heyefu 14:31 2018/4/15
+     **/
+    public void acceptString() {
+        try (ServerSocket server = new ServerSocket(8888);
+             Socket socket = server.accept();
+             InputStream in = socket.getInputStream();
+             DataInputStream dataInput = new DataInputStream(in)
+        ) {
+            String data = dataInput.readUTF();
+            System.out.printf("接收的字符串为:%s %n", data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Description:
+     * 利用Socket接收int数据
+     *
+     * @author heyefu 14:26 2018/4/15
+     **/
+    public void acceptInt() {
+        try (ServerSocket server = new ServerSocket(8888);
+             Socket socket = server.accept();
+             InputStream in = socket.getInputStream()
+        ) {
+            int msg = in.read();
+            System.out.printf("接收的数字为:%d %n", msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
--- src/test/java/com/heyfu/test/TestSocket.java
+++ src/test/java/com/heyfu/test/TestSocket.java
@@ -2,9 +2,12 @@ package com.heyfu.test;
 
 import org.junit.Test;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Description:
@@ -16,9 +19,33 @@ import java.net.Socket;
 public class TestSocket {
 
 
+    /**
+     * Description:
+     * 测试利用DataOutputStream发送字符串
+     *
+     * @author heyefu 14:42 2018/4/15
+     **/
     @Test
-    public void testClient(){
+    public void testSendString() {
+        try (Socket socket = new Socket("127.0.0.1", 8888);
+             OutputStream out = socket.getOutputStream();
+             DataOutputStream dataOut = new DataOutputStream(out)
+        ) {
+            dataOut.writeUTF("测试利用DataOutputStream及Socket发送字符串");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    /**
+     * Description:
+     * 测试利用OutputStream发送数字
+     *
+     * @author heyefu 14:41 2018/4/15
+     **/
+    @Test
+    public void testClientInt() {
         try {
             Socket socket = new Socket("127.0.0.1", 8888);
             OutputStream out = socket.getOutputStream();
@@ -28,8 +55,6 @@ public class TestSocket {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
