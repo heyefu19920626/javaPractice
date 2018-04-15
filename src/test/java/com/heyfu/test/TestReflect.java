95e765760478eb10c7641bce9e2d47fd60adc2c2
heyefu19920626 <527380349@qq.com>
2018-04-15 23:03:49 +0800

    reflect

---
+++ src/test/java/com/heyfu/test/TestReflect.java
@@ -0,0 +1,23 @@
+package com.heyfu.test;
+
+/**
+ * Description:
+ * 测试反射
+ *
+ * @author heyefu
+ * Create in: 2018-04-15
+ * Time: 下午10:21
+ **/
+public class TestReflect {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+}
